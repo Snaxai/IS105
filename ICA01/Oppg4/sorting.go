dce88a8e97188597dcfc86abab9168b855e0fedf
Sander <Sander@Users-MBP-2.uia.no>
2019-03-13 11:25:01 +0100

    bubble sort

--- ICA01/Oppg4/sorting.go
+++ ICA01/Oppg4/sorting.go
@@ -1,66 +1,37 @@
-package algorithms
+package main
 
-// Les https://en.wikipedia.org/wiki/Bubble_sort
-func Bubble_sort_modified(list []int) {
-	n := len(list)
-	for i := 0; i < n-1 && f; i++ {
-		f := 0
-		for j := i; j < n-1-i; j++ {
-			if list[j] > list[j+1] {
-				temp := list[j+1]
-				list[j+1] = list[j]
-				list[j] = temp
-				f := 1
-			}
-		}
-		for k := n - 2 - i; k > i && f; k-- {
-			if list[k] < list[k-1] {
-				temp := list[k-1]
-				list[k-1] = list[k]
-				list[k] = temp
-				f := 1
-			}
-		}
-	}
-}
+import (
+	"fmt"
+)
 
-// Implementering av Bubble_sort algoritmen
-func Bubble_sort(list []int) {
-	// find the length of list n
-	n := len(list)
-	for i := 0; i < n; i++ {
-		for j := 0; j < n-1; j++ {
-			if list[j] > list[j+1] {
-				temp := list[j+1]
-				list[j+1] = list[j]
-				list[j] = temp
+var toBeSorted [10]int = [10]int{1, 3, 2, 9, 4, 8, 6, 7, 5, 0}
+
+func bubbleSort(input [10]int) {
+
+	n := 10
+
+	swapped := true
+
+	for swapped {
+
+		swapped = false
+
+		for i := 1; i < n; i++ {
+
+			if input[i-1] > input[i-1] {
+
+				fmt.Println("Swapping")
+
+				input[i], input[i-1] = input[i-1], input[i]
+
+				swapped = true
 			}
 		}
 	}
+
+	fmt.Println(input)
 }
-
-// Implementering av Quicksort algoritmen
-func QSort(values []int) {
-	qsort(values, 0, len(values)-1)
-}
-
-func qsort(values []int, l int, r int) {
-	if l >= r {
-		return
-	}
-
-	pivot := values[l]
-	i := l + 1
-
-	for j := l; j <= r; j++ {
-		if pivot > values[j] {
-			values[i], values[j] = values[j], values[i]
-			i++
-		}
-	}
-
-	values[l], values[i-1] = values[i-1], pivot
-
-	qsort(values, l, i-2)
-	qsort(values, i, r)
+func main() {
+	fmt.Println("Hei på deg")
+	bubbleSort(toBeSorted)
 }
