5d6095b1b89e03684439686f4ae177e029122ead
Daniel <Daniel-mossestad@hotmail.com>
2019-05-05 16:31:40 +0200

    ascii oppg1

---
+++ ICA02/Oppg1/ascii_main/ascii_main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/Snaxai/IS105/ICA02/Oppg1/ascii"
+
+func main() {
+	ascii.IterateOverASCIIStringLiteral()
+	ascii.GetASCIIStringLiteral()
+	ascii.GreetingASCII()
+}
---
+++ ICA02/Oppg1/ascii_mainA/ascii_mainA.go
@@ -0,0 +1,8 @@
+package main
+
+import "github.com/Snaxai/IS105/ICA02/Oppg1/ascii"
+
+func main() {
+	ascii.IterateOverASCIIStringLiteral()
+	ascii.GetASCIIStringLiteral()
+}
