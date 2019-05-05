8fccbcd346c273af282cfc1a395cfb3b6ee75780
Snaxai <47025192+Snaxai@users.noreply.github.com>
2019-05-05 16:32:09 +0200

    Delete ascii_main.go

--- ICA02/Oppg1/ascii_main.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "github.com/Snaxai/IS105/ICA02/Oppg1/ascii"
-
-func main() {
-	ascii.IterateOverASCIIStringLiteral()
-	ascii.GetASCIIStringLiteral()
-	ascii.GreetingASCII()
-}
