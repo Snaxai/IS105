f0e07d6a5d699228e889a9ab10817e1a57756c71
Snaxai <Daniel-mossestad@hotmail.com>
2019-05-07 14:17:26 +0200

    frequence oppg 3

---
+++ ICA03/Oppg3/frequence/frequence_main/frequence_main.go
@@ -0,0 +1,10 @@
+package main
+
+import (
+	"github.com/Snaxai/IS105/ICA03/Oppg3/frequence"
+)
+
+func main() {
+	frequence.FileinfoFrequence("pg100.txt")
+	frequence.FileToByteslice("pg100.txt")
+}
