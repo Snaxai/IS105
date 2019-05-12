c65aec4ba8d473f5c1a46803a6b489cc166d3cee
Daniel <Daniel-mossestad@hotmail.com>
2019-05-12 22:14:17 +0200

    oppg3 ica02

--- ICA02/Oppg3/Oppg3B_main/Oppg3B_main.go
+++ ICA02/Oppg3/Oppg3B_main/Oppg3B_main.go
@@ -1,9 +1,10 @@
 package main
 
-import "github.com/sanderkvale/IS105/ICA02/Oppg3/fileutils"
+import "github.com/Snaxai/IS105/ICA02/Oppg3/fileutils"
 
 func main() {
 	//fileutils.FileToByteslice("lang01.wl")
 	//fileutils.FileToByteslice("lang02.wl")
 	fileutils.FileToByteslice("lang03.wl")
+	fileutils.Print()
 }
--- ICA02/Oppg3/fileutils/fileutils.go
+++ ICA02/Oppg3/fileutils/fileutils.go
@@ -7,6 +7,18 @@ import (
 	"os"
 )
 
+const tb = "\xEF\xDA\xA3\xD2\xD3\xCB\x0A\xEF\xDA\xA3\xD2\xD3\xCB\xC1\x0A\xEF" +
+	"\x0A\x0A\xFE\xFD\x73\x6B\x61\x72\x0A\xFE\xFD\x73\x6B\x61\x72\x61\x6E\x61" +
+	"\x0A\x0A\xF8\x79\x65\x73\x70\x65\x73\x69\x61\x6C\x69\x73\x74\x65\x6E\x0A"
+
+//Print Konverterer og printer ut	const tb
+func Print() {
+	for i := 0; i < len(tb); i++ {
+		//fmt.Printf("%q", tb[i])
+		fmt.Printf("%c", tb[i])
+	}
+}
+
 // FileToByteslice har en "string literal" som argument
 // og returnerer en "slice"
 func FileToByteslice(filename string) []byte {
